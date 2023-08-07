d541685ba68079b084c939c47eda1a2be1a3a6a1
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-07 13:56:59 +0530

    Featured section completed along with the dropdown genre menu

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,13 +5,27 @@ export default function Featured({type}) {
   return (
     <div className='featured'>
 
-            {type &&(
-                <div className="category">
-                    <span>{type==="movie" ? title="Movies": "Series"}</span>
-                    <select name="genre" id="genre"></select>
-                </div>
-
-            )}
+                {type && (
+                        <div className="category">
+                        <span>{type === "movie" ? "Movies" : "Series"}</span>
+                        <select name="genre" id="genre">
+                            <option>Genre</option>
+                            <option value="adventure">Adventure</option>
+                            <option value="comedy">Comedy</option>
+                            <option value="crime">Crime</option>
+                            <option value="fantasy">Fantasy</option>
+                            <option value="historical">Historical</option>
+                            <option value="horror">Horror</option>
+                            <option value="romance">Romance</option>
+                            <option value="sci-fi">Sci-fi</option>
+                            <option value="thriller">Thriller</option>
+                            <option value="western">Western</option>
+                            <option value="animation">Animation</option>
+                            <option value="drama">Drama</option>
+                            <option value="documentary">Documentary</option>
+                        </select>
+                        </div>
+                    )}
         <img src='https://w0.peakpx.com/wallpaper/407/697/HD-wallpaper-interstellar-movie-wide-interstellar-movies.jpg'  alt='featured' />
         <div className="info">
             <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/0/02/Interstellar.svg/1280px-Interstellar.svg.png" alt="Movie logo" />
